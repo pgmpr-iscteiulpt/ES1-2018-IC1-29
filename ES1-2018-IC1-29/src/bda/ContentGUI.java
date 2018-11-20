d158c07bc724d662f11d48ab9680a2c0a94e586d
pgmpr-iscteiulpt <pgmpr@iscte-iul.pt>
2018-11-20 18:02:01 +0000

    Início da implementação da interface de leitura do conteúdo selecionado

--- ES1-2018-IC1-29/src/bda/ContentGUI.java
+++ ES1-2018-IC1-29/src/bda/ContentGUI.java
@@ -1,16 +1,31 @@
 package bda;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.HeadlessException;
+import java.awt.Image;
+import java.awt.TextField;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+
+import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 public class ContentGUI {
 
@@ -20,22 +35,56 @@ public class ContentGUI {
 	private final int height = (int) (0.9 * screenDim.height);
 	private final int widthLocation = screenDim.width / 2 - width / 2;
 	private final int heightLocation = screenDim.height / 2 - height / 2;
+	
+	private JPanel north = new JPanel(); //*
+	private JPanel center = new JPanel(); //*
+	private JPanel south = new JPanel(); //*
+	private JButton respond; //*
+	private JLabel sender; //*
+	private JTextArea text; //*
+	private JButton send; //*
 
 	public ContentGUI(Message m) throws HeadlessException, MessagingException {
 		frame = new JFrame(m.getSubject().toString());
 		frame.setSize(width, height);
 		frame.setLocation(widthLocation, heightLocation);
-
+		
+		frame.setLayout(new BorderLayout()); //*
+		north.setLayout(new BorderLayout()); //*
+		north.setPreferredSize(new Dimension(0, 60)); 
+		
+		Address[] froms = m.getFrom(); //*
+		
+		sender = new JLabel("From:   " + ((InternetAddress) froms[0]).getAddress()); //*
+		sender.setFont(new Font("Arial", Font.PLAIN, 18));
+		north.add(sender, BorderLayout.WEST); //*
+		center.setLayout(new BorderLayout()); //*
+		south.setLayout(new BorderLayout()); //*
+		south.setPreferredSize(new Dimension(0, 0)); //*
+		
+		ImageIcon icon = new ImageIcon("images/" + "res" + ".png");
+		Image image = icon.getImage().getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH);
+		icon = new ImageIcon(image);
+		
+		respond = new JButton("Respond" , icon); //*
+		respond.setPreferredSize(new Dimension(50,50)); //*
+		center.add(respond , BorderLayout.SOUTH); //*
+	//	south.add(respond , BorderLayout.NORTH); //*
+		
+				
+		addOperations(); //*
+		
 		JTextArea t = new JTextArea();
 		t.setEditable(false);
 		String hash = createHashCode(m);
+		t.append("\n");//*
 		
 		try (Scanner scanner = new Scanner(
 				new FileReader(new File(System.getProperty("user.dir") + File.separator + "Emails\\Email" + hash)))) {
 			String s;
 			while (scanner.hasNextLine()) {
 				s = scanner.nextLine(); 
-				t.append(s + "\n");
+				t.append(" "+ s + "\n");
 			}
 		scanner.close();
 			
@@ -43,8 +92,44 @@ public class ContentGUI {
 			e.printStackTrace();
 		}
 		
-		frame.add(t);
+		t.setFont(new Font("Arial", Font.PLAIN, 14));
+		center.add(t); //*
+		
+		frame.add(north, BorderLayout.NORTH); frame.add(center, BorderLayout.CENTER); frame.add(south, BorderLayout.SOUTH); //*
+		//frame.add(t);
 		frame.setVisible(true);
+	}
+	
+	public void addOperations() {
+		respond.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				south.setPreferredSize(new Dimension(200, 200));
+				
+				text = new JTextArea();
+				text.setPreferredSize(new Dimension(300,300));
+			
+				ImageIcon icon = new ImageIcon("images/s.png");
+				Image image = icon.getImage().getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH);
+				icon = new ImageIcon(image);
+						
+				send = new JButton("Send" , icon);
+				send.setPreferredSize(new Dimension(200,50));
+				
+				south.add(text, BorderLayout.CENTER);
+				JPanel n = new JPanel();
+				n.setLayout(new BorderLayout());
+				n.setPreferredSize(new Dimension(200,50));
+				n.add(send, BorderLayout.SOUTH);
+				south.add(n, BorderLayout.EAST);
+				
+				frame.add(south, BorderLayout.SOUTH);
+				frame.invalidate();
+				frame.validate();
+				frame.repaint();
+			}
+		});
 	}
 
 	private String createHashCode(Message m) throws MessagingException {
