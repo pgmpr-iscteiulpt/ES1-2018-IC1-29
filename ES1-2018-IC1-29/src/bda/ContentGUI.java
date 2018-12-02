663e0d6cb6ca08191438ca0d0e4c3927dedda38b
mccta-iscteiulpt <mccta@iscte-iul.pt>
2018-12-02 23:26:33 +0000

    Implementação da visualização de likes e comments nos posts

--- ES1-2018-IC1-29/src/bda/ContentGUI.java
+++ ES1-2018-IC1-29/src/bda/ContentGUI.java
@@ -12,6 +12,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 import javax.mail.MessagingException;
 import javax.swing.ImageIcon;
@@ -20,6 +21,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.xml.ws.soap.AddressingFeature;
 
 /**
  * Cria uma JFrame representativa da interface referente apenas ao conteúdo
@@ -47,7 +49,7 @@ public class ContentGUI {
 	private JTextArea text;
 	private JButton send;
 
-	private long id;
+	private Object id;
 	private ArrayList<Object> handlers = new ArrayList<>();
 
 	/**
@@ -85,22 +87,30 @@ public class ContentGUI {
 		Image image = icon.getImage().getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH);
 		icon = new ImageIcon(image);
 
-		reply = new JButton("Reply", icon);
-		reply.setPreferredSize(new Dimension(50, 50));
+		if (content.getType().equals("Facebook")) {
+			JButton getLikesComments = new JButton("Likes and Comments");
+			getLikesComments.setPreferredSize(new Dimension(50, 50));
+			center.add(getLikesComments, BorderLayout.SOUTH);
+			addFacebookOperations(getLikesComments);
 
-		if (content.getType().equals("Twitter")) {
-			JPanel buttons = new JPanel();
-			JButton retweet = new JButton("Retweet");
-			reply.setPreferredSize(new Dimension((int) (width * 0.2), (int) (height * 0.05)));
-			retweet.setPreferredSize(new Dimension((int) (width * 0.2), (int) (height * 0.05)));
-			addRetweetOperation(retweet);
-			buttons.add(reply);
-			buttons.add(retweet);
-			center.add(buttons, BorderLayout.SOUTH);
 		} else {
-			center.add(reply, BorderLayout.SOUTH);
+			reply = new JButton("Reply", icon);
+			reply.setPreferredSize(new Dimension(50, 50));
+
+			if (content.getType().equals("Twitter")) {
+				JPanel buttons = new JPanel();
+				JButton retweet = new JButton("Retweet");
+				reply.setPreferredSize(new Dimension((int) (width * 0.2), (int) (height * 0.05)));
+				retweet.setPreferredSize(new Dimension((int) (width * 0.2), (int) (height * 0.05)));
+				addRetweetOperation(retweet);
+				buttons.add(reply);
+				buttons.add(retweet);
+				center.add(buttons, BorderLayout.SOUTH);
+			} else {
+				center.add(reply, BorderLayout.SOUTH);
+			}
+			addReplyOperations(from.toString());
 		}
-		addOperations(from.toString());
 
 		JTextArea t = new JTextArea();
 		t.setEditable(false);
@@ -120,6 +130,8 @@ public class ContentGUI {
 					new FileReader(new File(System.getProperty("user.dir") + File.separator + hash)));
 			if (content.getType().equals("Twitter"))
 				id = Long.valueOf(scanner.nextLine());
+			if (content.getType().equals("Facebook"))
+				id = scanner.nextLine();
 			String s;
 			t.append("\n");
 			t.append("\n" + "ASSUNTO:   " + content.getSubject().toString() + "\n" + "\n");
@@ -150,7 +162,43 @@ public class ContentGUI {
 						twitter = (FetchTweets) cH;
 				}
 
-				twitter.retweet(id);
+				twitter.retweet((long) id);
+
+			}
+		});
+
+	}
+
+	private void addFacebookOperations(JButton getLikesComments) {
+		getLikesComments.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				south.setPreferredSize(new Dimension(200, 200));
+
+				text = new JTextArea();
+				text.setPreferredSize(new Dimension(300, 300));
+				south.add(text, BorderLayout.CENTER);
+
+				long likesCount = 0;
+				long commentsCount = 0;
+				HashMap<String, String> comments = new HashMap<>();
+				FetchPosts facebook = null;
+				for (Object cH : handlers) {
+					if (cH instanceof FetchPosts)
+						facebook = (FetchPosts) cH;
+				}
+
+				likesCount = facebook.getLikesCount((String) id);
+				commentsCount = facebook.getCommentsCount((String) id);
+				comments = facebook.getComments((String) id);
+				text.setText("Número de likes:  " + likesCount + "\n" + "\n");
+				text.append("Comentários: " + commentsCount + "\n");
+				for (HashMap.Entry<String, String> entry : comments.entrySet()) {
+					text.append("De:  " + entry.getValue() + "\n");
+					text.append(entry.getKey() + "\n" + "\n");
+				}
 
 			}
 		});
@@ -161,7 +209,7 @@ public class ContentGUI {
 	 * Método que possibilita a resposta por parte do utilizador para o remetente do
 	 * conteúdo
 	 */
-	public void addOperations(String from) {
+	private void addReplyOperations(String from) {
 		reply.addActionListener(new ActionListener() {
 
 			@Override
@@ -197,7 +245,7 @@ public class ContentGUI {
 										twitter = (FetchTweets) cH;
 								}
 
-								twitter.replyTweet(id, text.getText());
+								twitter.replyTweet((long) id, text.getText());
 
 							}
 						}
--- ES1-2018-IC1-29/src/bda/FetchPosts.java
+++ ES1-2018-IC1-29/src/bda/FetchPosts.java
@@ -4,11 +4,16 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import javax.mail.MessagingException;
 import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import com.restfb.types.Comment;
+import com.restfb.types.Comments;
+import com.restfb.types.Likes;
 import com.restfb.types.Post;
 import com.restfb.types.User;
 
@@ -16,19 +21,16 @@ public class FetchPosts {
 
 	private User user;
 	private String userName;
-	FacebookClient a;
+	private FacebookClient fbClient;
 	private Connection<Post> result;
 	private ArrayList<Content> posts = new ArrayList<>();
 
+	@SuppressWarnings("deprecation")
 	private void setProperties() {
-		String accessToken = "EAALFIGPljFgBAGzUGaoJ5bux0MdV831tF9yeExTeWU7ap3xAMWMQnT6J3y8FhZCVXKL3CmXsN8K0TWXXRcWVdXgc0ddZAqdSejiZCwSYqoHoSS7LRcIcFfnR4ltYJQkgpvu4XP57GsflRGZBf5lxPctGQxzzIAhkytSZB6J0OxGZC9mgmHuYTv";
-		@SuppressWarnings("deprecation")
-		FacebookClient fbClient = new DefaultFacebookClient(accessToken);
-		a = fbClient;
+		String accessToken = "EAAD4Yt8YSVgBAJE0EEG5Aj4ktyavf4QcG2ZAnRq8tv6O6jAO5jfuqCXw5tCHyyZCItR6iJ4Fq74CYJr4yNjd9ZBS8oREAhyLmp6ClzUag7JleMZCUSPJojaPFMEPfYMQx175BNqUsEmCR745dghiFl41o2BpV46sgUill5AojVHf34lwzkqf";
+		fbClient = new DefaultFacebookClient(accessToken);
 		user = fbClient.fetchObject("me", User.class);
-
 		result = fbClient.fetchConnection("me/feed", Post.class);
-
 	}
 
 	public void checkPosts() {
@@ -43,6 +45,7 @@ public class FetchPosts {
 					hash = p.getHashCode();
 					PrintWriter writer = new PrintWriter(
 							System.getProperty("user.dir") + File.separator + "Resources\\Posts\\Post" + hash, "UTF-8");
+					writer.println(aPost.getId());
 					writer.println(aPost.getMessage());
 					writer.close();
 				} catch (MessagingException | IOException e) {
@@ -60,6 +63,30 @@ public class FetchPosts {
 
 	public String getUserName() {
 		return userName;
+	}
+
+	public long getLikesCount(String postID) {
+		Likes likes = fbClient.fetchObject(postID + "/likes", Likes.class, Parameter.with("summary", 1),
+				Parameter.with("limit", 0));
+		return likes.getTotalCount();
+	}
+
+	public long getCommentsCount(String postID) {
+		Comments comments = fbClient.fetchObject(postID + "/comments", Comments.class, Parameter.with("summary", 1),
+				Parameter.with("limit", 0));
+		return comments.getTotalCount();
+
+	}
+
+	public HashMap<String, String> getComments(String postID) {
+		HashMap<String, String> comments = new HashMap<>();
+		Connection<Comment> allComments = fbClient.fetchConnection(postID + "/comments", Comment.class);
+		for (List<Comment> postcomments : allComments) {			
+			for (Comment comment : postcomments) {
+				comments.put(comment.getMessage().toString(), comment.getFrom().getName());
+			}
+		}
+		return comments;
 	}
 }
 
